67c15624281fc197e2cf12f914b1ede1d2d50afb
amanda-ar4 <amanda.raraujo10@senacsp.edu.br>
2025-02-26 19:55:37 +0200

    Upload de códigos da primeira aula

---
+++ Aula01/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int idade = 21;
+
+            string nomeCompleto = "Amanda Ramos Araujo";
+
+            var variavelSemTipo = "Amanda";
+
+            
+
+            Console.WriteLine(variavelSemTipo.GetType());
+
+            
+            Console.WriteLine("Digite seu nome: ");
+            string nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine("Digite a sua idade: ");
+            string idadeUsuario = Console.ReadLine();
+
+
+
+
+
+
+
+
+
+            Console.WriteLine($"Meu nome é {nomeUsuario} e a minha idade é {idadeUsuario}");
+
+        }
+    }
+}
